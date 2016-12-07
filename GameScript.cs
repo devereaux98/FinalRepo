7677ff3fe934781f3552431c669fafb3defa257c
Devereaux Legakis <devereaux98@verizon.net>
2016-12-07 11:59:17 -0700

    Commit

--- GameScript.cs
+++ GameScript.cs
@@ -1,17 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class BattleScript : MonoBehaviour {
+public class GameScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () 
-	{
-	
+    {
+        String Enemy = ("Spider");
 	}
 	
 	// Update is called once per frame
 	void Update () 
-	{
+    {
 	
 	}
 }
