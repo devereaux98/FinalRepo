1a42e9122f1f6a79eb591f1bad4f4265cd160ecf
Devereaux Legakis <devereaux98@verizon.net>
2016-12-07 11:13:06 -0700

    Commit

---
+++ Display.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Display : MonoBehaviour {
+
+	// Use this for initialization
+	Text instruction;
+	void Start () 
+	{
+		instruction = GetComponent<Text>();
+		instruction.text = ("A ghost appeared!");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
