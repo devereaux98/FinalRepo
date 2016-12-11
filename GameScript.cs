fbfc8d5bc77f2206acd0c93d0960a0214a3903eb
Devereaux Legakis <devereaux98@verizon.net>
2016-12-10 17:57:01 -0700

    c

--- GameScript.cs
+++ GameScript.cs
@@ -1,24 +1,5 @@
-using UnityEngine;
-using System.Collections;
-
-public class GameScript : MonoBehaviour 
+using System;
+public class GameScript 
 {
-	public static string Enemy = ("Spider");
-    public static bool Walk;
     
-    // Use this for initialization
-    void Start () 
-    {
-        string Enemy = ("Spider");
-        bool Walk = false;
-        GameObject Player10 = GameObject.Find("Player10");
-	}
-	// Update is called once per frame
-	void Update () 
-    {
-        while (Walk = false)
-        {
-             gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Dungeon1.png");
-        }
-	}
 }
